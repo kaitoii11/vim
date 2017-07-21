7897e5522cdc67b0fee102cfb0426829c4c6fc18
Kaito Ii <kaitoii1111@gmail.com>
2017-07-21 05:25:24 +0000

    fixed installation process

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ install_vimrc() {
   [ -e "~/.vim" ] && die "~/.vim already exist"
   [ -e "~/.vimrc" ] && die "~/.vimrc already exist"
 
-  git clone git://github.com/kaitoii11/vim.git
-  cd vim
+ # git clone git://github.com/kaitoii11/vim.git
+ # cd vim
 
   git submodule init
   git submodule update
