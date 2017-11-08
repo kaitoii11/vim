80cbced8d4743641970251ea8dc829eb1ef20c09
Kaito Ii <kaitoii1111@gmail.com>
2017-11-08 03:41:36 +0000

    fixed install process

--- install.sh
+++ install.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
 install_vimrc() {
-  [ -e "$HOME/.vim" ] && die "$HOME/.vim already exist"
-  [ -e "$HOME/.vimrc" ] && die "$HOME/.vimrc already exist"
+  [ -e "$HOME/.vim" ] && echo "$HOME/.vim already exist" && exit
+  [ -e "$HOME/.vimrc" ] && echo "$HOME/.vimrc already exist" && exit
 
   git submodule init
   git submodule update
 
-  ln -s  "$PWD/.vimrc" "$HOME/.vimrc"
+  MYPATH=$PWD
+  cd $HOME
+  mv $MYPATH $HOME/.vim
+  ln -s  "$HOME/.vim/.vimrc" "$HOME/.vimrc"
 }
 
 install_vimrc
